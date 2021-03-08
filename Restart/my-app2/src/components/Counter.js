51f5736b61b8c0f90fb69272d384ee5509dc58f2
Cameron O'Brien <cameron.obrien@selu.edu>
2021-03-07 23:43:36 -0600

    No negatives in counter.js

--- Restart/my-app2/src/components/Counter.js
+++ Restart/my-app2/src/components/Counter.js
@@ -4,7 +4,7 @@ class Counter extends Component {
     constructor(props) {
     super(props);
     this.state = {
-        count: 0
+        count: 0 
     };
 }
 
@@ -15,9 +15,15 @@ increment = () => {
 };
 
 decrement = () => {
-    this.setState({
-        count: this.state.count - 1
-    });
+    if(this.state.count <= 0){
+        this.setState({
+            count: 0
+        });
+    }else {
+        this.setState({
+            count: this.state.count - 1
+        });
+    }
 };
 
 render() {
