74dee00efdad1e643779caa4adc57bd32b044f83
Cameron O'Brien <cameron.obrien@selu.edu>
2021-03-23 21:25:26 -0500

    Added pictures to home

--- SIgnYourYard/SIgnYourYard/Restart/my-app2/src/components/Home.js
+++ SIgnYourYard/SIgnYourYard/Restart/my-app2/src/components/Home.js
@@ -1,17 +1,26 @@
 import React from 'react'
-
-import YardsignOne from '../YardsignOne.jpg'
+import YardsignOne from '../Pictures/YardsignOne.jpg'
+import YardsignTwo from '../Pictures/YardsignTwo.jpg'
+import YardsignThree from '../Pictures/YardsignThree.jpg'
+import YardsignFour from '../Pictures/YardsignFour.jpg'
 
 function Home() {
     return (
         <div className="gradient">
             <div className="buffer">
-                    <div classname="home">
-                    <h1 classname="home">Welcome to</h1>
-                    <h1 classname="home">Sign Your Yard</h1>
+                <div classname="home">
+                    <p className= "title">Welcome to </p>
+                    <p className= "title">Sign Your Yard</p>
+                    <p className="label">Gallery</p>
+                    <div className= "pad">
+                        <div>
+                            <img alt="yard sign #1" className="images" src={YardsignOne}/>
+                            <img alt="yard sign #2" className="images" src={YardsignTwo}/>
+                            <img alt="yard sign #3" className="images" src={YardsignThree}/>
+                            <img alt="yard sign #4" className="images" src={YardsignFour}/>
+                        </div>
                     </div>
-               
-                <img alt="yard sign #3" className="images" src={YardsignOne}/>
+                </div>
             </div>
         </div>
     )
