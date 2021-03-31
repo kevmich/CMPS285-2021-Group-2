015acf5567dcc4aae139a37a322b2a9bd1e8e31e
Cameron O'Brien <cameron.obrien@selu.edu>
2021-03-30 22:42:37 -0500

    Carousel working

--- SIgnYourYard/SIgnYourYard/Restart/my-app2/src/components/Home.js
+++ SIgnYourYard/SIgnYourYard/Restart/my-app2/src/components/Home.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import YardsignOne from '../Pictures/YardsignOne.jpg'
-import YardsignTwo from '../Pictures/YardsignTwo.jpg'
-import YardsignThree from '../Pictures/YardsignThree.jpg'
-import YardsignFour from '../Pictures/YardsignFour.jpg'
+import Carousel from './Carousel';
+
 
 function Home() {
     return (
@@ -12,18 +10,14 @@ function Home() {
                 <p className= "title">Sign Your Yard</p>
                 <p className="label">Gallery</p>
                 <div className= "pad">
-                    <div>
-                        <img alt="yard sign #1" className="images" src={YardsignOne}/>
-                        <img alt="yard sign #2" className="images" src={YardsignTwo}/>
-                    </div>
-                    <div>
-                        <img alt="yard sign #3" className="images" src={YardsignThree}/>
-                        <img alt="yard sign #4" className="images" src={YardsignFour}/>
-                    </div>
+                        
+                       <Carousel className= "images"> 
+                       </Carousel>
+                    
                 </div>
             </div>
         </div>
     )
 }
 
-export default Home
+export default Home;
